790bbe68a335571ed678fc668d0c52b950f73ecc
VIVEK <99870677+Roninopp@users.noreply.github.com>
2023-01-02 00:44:59 +0530

    Update start.py

--- AnonX/plugins/start.py
+++ AnonX/plugins/start.py
@@ -31,7 +31,7 @@ loop = asyncio.get_running_loop()
 
 
 @app.on_message(
-    filters.command(get_command("START_COMMAND"))
+    filters.command(get_command("VCSTART_COMMAND"))
     & filters.private
     & ~filters.edited
     & ~BANNED_USERS
